161c7400c84eeb920868af50e001b1ba033a601f
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2024-03-25 08:34:24 +0100

    refactor update functionality

--- component/UpdateQuestionForm2.tsx
+++ component/UpdateQuestionForm2.tsx
@@ -1,74 +1,8 @@
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { useAppDispatch } from '../redux/store/hooks';
-// import { updateQuestionAsync } from '../redux/thunks/updateQuestionThunks';
-
-// interface UpdateQuestionFormProps {
-//     questionId: string;
-//     initialQuestion: string;
-//     initialOptions: string[];
-//     onClose: () => void;
-//   }
-  
-//   const UpdateQuestionForm2: React.FC<UpdateQuestionFormProps> = ({
-//     questionId,
-//     initialQuestion,
-//     initialOptions,
-//     onClose,
-//   }) => {
-//   const dispatch = useAppDispatch();
-//   const [question, setQuestion] = useState(initialQuestion);
-//   const [options, setOptions] = useState(initialOptions);
-
-//   console.log(questionId,'questionID @ edit form=')
-//   const handleQuestionChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-//     setQuestion(e.target.value);
-//   };
-
-//   const handleOptionChange = (index: number, value: string) => {
-//     const updatedOptions = [...options];
-//     updatedOptions[index] = value;
-//     setOptions(updatedOptions);
-//   };
-
-//   const handleSubmit = () => {
-//     // Dispatch action to update question
-//     dispatch(updateQuestionAsync({ id: questionId, question, options }));
-//     onClose(); // Close the form after submitting
-//   };
-
-//   return (
-//     <div>
-//       <h3>Edit Question:</h3>
-//       <input type="text" value={question} onChange={handleQuestionChange} />
-//       <h4>Edit Options:</h4>
-//       {options.map((option, index) => (
-//         <div key={index}>
-//           <input
-//             type="text"
-//             value={option}
-//             onChange={(e) => handleOptionChange(index, e.target.value)}
-//           />
-//         </div>
-//       ))}
-//       <button onClick={handleSubmit}>Save Changes</button>
-//       <button onClick={onClose}>Cancel</button>
-//     </div>
-//   );
-// };
-
-// export default UpdateQuestionForm2;
-
-
 // UpdateQuestionForm2.tsx
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppDispatch } from '../redux/store/hooks';
-import { updateQuestionAsync } from '../redux/thunks/updateQuestionThunks';
-import { fetchQuestions } from '@/redux/thunks/questionsThunks';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import { RootState } from '@/redux/store';
+import { updateQuestion} from '../redux/thunks/updateQuestionThunks';
 
 interface UpdateQuestionFormProps {
   questionId: string;
@@ -86,7 +20,6 @@ const UpdateQuestionForm2: React.FC<UpdateQuestionFormProps> = ({
   const dispatch = useAppDispatch();
   const [question, setQuestion] = useState(initialQuestion);
   const [options, setOptions] = useState(initialOptions);
-// console.log(questionId,'questionId=== form2')
   const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuestion(e.target.value);
   };
@@ -99,9 +32,7 @@ const UpdateQuestionForm2: React.FC<UpdateQuestionFormProps> = ({
 
 
   const handleSubmit = () => {
-    // console.log({ id: questionId, question, options }); // Logging the payload
-    dispatch(updateQuestionAsync({ id: questionId, question, options }));
-    // dispatch(fetchQuestions)
+    dispatch(updateQuestion({ id: questionId, question, options }));
     onClose();
   };
 
@@ -126,3 +57,4 @@ const UpdateQuestionForm2: React.FC<UpdateQuestionFormProps> = ({
 };
 
 export default UpdateQuestionForm2;
+
--- redux/thunks/updateQuestionThunks.ts
+++ redux/thunks/updateQuestionThunks.ts
@@ -1,6 +1,5 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios, { AxiosError } from 'axios';
-import { RootState, AppThunk } from '../store'; // Import AppThunk from store
+import axios from 'axios';
+import { RootState, AppThunk } from '../store';
 import { setLoading, setError } from '../slices/updateQuestionSlice';
 import { fetchQuestions } from './questionsThunks';
 
@@ -10,33 +9,29 @@ interface UpdateQuestionData {
   options: string[];
 }
 
-export const updateQuestionAsync = createAsyncThunk(
-  'updateQuestion/updateQuestionAsync',
-  async ({ id, question, options }: UpdateQuestionData, { getState, dispatch }) => {
-    try {
-      dispatch(setLoading(true));
-      const token = (getState() as RootState).token.value;
-      await axios.put(
-        `https://qt.organogram.app/questions/${id}`,
-        { question, options },
-        {
-          headers: {
-            Token: token,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      dispatch(setLoading(false));
-      // dispatch(fetchQuestions());
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        dispatch(setError(error.message));
-      } else {
-        dispatch(setError('An error occurred'));
+export const updateQuestion = (data: UpdateQuestionData): AppThunk => async (dispatch, getState) => {
+  try {
+    dispatch(setLoading(true));
+    const token = (getState() as RootState).token.value;
+    await axios.put(
+      `https://qt.organogram.app/questions/${data.id}`,
+      { question: data.question, options: data.options },
+      {
+        headers: {
+          Token: token,
+          'Content-Type': 'application/json',
+        },
       }
-    } finally {
-      dispatch(setLoading(false));
+    );
+    dispatch(setLoading(false));
+    dispatch(fetchQuestions());
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      dispatch(setError(error.message));
+    } else {
+      dispatch(setError('An error occurred'));
     }
+  } finally {
+    dispatch(setLoading(false));
   }
-);
-
+};
